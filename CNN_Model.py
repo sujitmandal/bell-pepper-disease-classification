64b6160b675031947bece76f493d8f723e39681d
Sujit Mandal <49348037+sujitmandal@users.noreply.github.com>
2021-08-24 23:02:47 +0530

    added CNN_model

---
+++ CNN_Model.py
@@ -0,0 +1,219 @@
+
+# Author : Sujit Mandal
+import os
+import json
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow.keras import models
+from tensorflow.keras import layers
+from tensorflow.python.keras.layers.preprocessing.image_preprocessing import RandomRotation
+
+# Github: https://github.com/sujitmandal
+# Pypi : https://pypi.org/user/sujitmandal/
+# LinkedIn : https://www.linkedin.com/in/sujit-mandal-91215013a/
+
+EPOCHS = 1
+CHANNELS = 3
+BATCH_SIZE = 32
+IMAGE_SIZE = 256
+
+
+
+dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    "dataset",
+    seed = 123,
+    shuffle=True,
+    image_size=(IMAGE_SIZE, IMAGE_SIZE),
+    batch_size=BATCH_SIZE
+)
+
+
+class_names = dataset.class_names
+
+# print(class_names)
+
+# plt.figure(figsize=(10, 10))
+# for batch, label in dataset.take(1):
+#     for i in range(16):
+#         ax = plt.subplot(4, 4, i + 1)
+#         plt.imshow(batch[i].numpy().astype("uint8"))
+#         plt.title(class_names[label[i]])
+#         plt.axis("off")
+#     plt.show()
+
+
+def split_dataset(dataset, train_split, val_split, test_split):
+    # shuffle = True
+    # shuffle_size = 10000
+    assert(train_split + val_split + test_split) == 1
+
+    dataset_size = len(dataset)
+
+    # if shuffle:
+    #     dataset = dataset.shuffle(shuffle_size, seed=12)
+
+    train_size = int(train_split * dataset_size)
+    val_size = int(val_split * dataset_size)
+
+    train_dataset = dataset.take(train_size)
+    val_dataset = dataset.skip(train_size).take(val_size)
+    test_dataset = dataset.skip(train_size).skip(val_size)
+
+    return(train_dataset, val_dataset, test_dataset)
+
+
+
+train_split = 0.8
+val_split = 0.1
+test_split = 0.1
+
+train_dataset, val_dataset, test_dataset = split_dataset(dataset, train_split, val_split, test_split)
+
+print('\n')
+print('Total dataset : {}'.format(len(dataset)))
+print('Train dataset : {}'.format(len(train_dataset)))
+print('Val dataset : {}'.format(len(val_dataset)))
+print('Test dataset : {}'.format(len(test_dataset)))
+
+
+# Cache, Shuffle, and Prefetch the Dataset
+
+# train_dataset = train_dataset.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+# val_dataset = val_dataset.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+# test_dataset = test_dataset.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+
+
+#Resizing and Normalization
+data_resize_rescale = tf.keras.Sequential([
+    layers.experimental.preprocessing.Resizing(IMAGE_SIZE, IMAGE_SIZE),
+    layers.experimental.preprocessing.Rescaling(1./255)
+])
+
+# Data Augmentation
+data_augmentation = tf.keras.Sequential([
+    layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
+    layers.experimental.preprocessing.RandomRotation(0.2)
+])
+
+
+# build CNN model
+input_shape = (BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, CHANNELS)
+total_class = 2
+
+model = models.Sequential([
+    data_resize_rescale,
+    data_augmentation,
+
+    layers.Conv2D(32, kernel_size = (3, 3), activation = 'relu', input_shape= input_shape),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, kernel_size = (3,3), activation = 'relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, kernel_size = (3,3), activation = 'relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, kernel_size = (3, 3), activation = 'relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, kernel_size = (3, 3), activation = 'relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, kernel_size = (3, 3), activation = 'relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Flatten(),
+    layers.Dense(64, activation = 'relu'), 
+    layers.Dense(total_class, activation = 'softmax'),
+])
+
+model.build(input_shape=input_shape)
+print('\n')
+print('Model :')
+model.summary()
+
+# compiling the model
+
+model.compile(
+    optimizer='adam',
+    loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+    metrics=['accuracy']
+)
+
+history = model.fit(
+    train_dataset,
+    batch_size = BATCH_SIZE,
+    validation_data = val_dataset,
+    verbose = 1,
+    epochs = EPOCHS,
+)
+
+model_score = model.evaluate(test_dataset)
+
+print('\n')
+print(model_score)
+print(history.params)
+print(history.history.keys())
+
+
+model_performance = history.history
+print(json.dumps(model_performance, indent=4))
+
+
+accuracy = history.history['accuracy']
+val_accuracy = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(range(EPOCHS), accuracy, label='Training Accuracy')
+plt.plot(range(EPOCHS), val_accuracy, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(range(EPOCHS), loss, label='Training Loss')
+plt.plot(range(EPOCHS), val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+
+# Test model
+for image_batch, labels in test_dataset.take(1):
+    image = image_batch[0].numpy().astype("uint8")
+    label = labels[0].numpy()
+    
+    print('Image to Predict :')
+    plt.imshow(image)
+    print('Actual Lable : ', class_names[label])
+    
+    batch_prediction = model.predict(image_batch)
+    print('Predicted Label : ', class_names[np.argmax(batch_prediction[0])])
+
+
+def predict(model, img):
+    img_array = tf.keras.preprocessing.image.img_to_array(images[i].numpy())
+    img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+    predictions = model.predict(img_array)
+    predicted_class = class_names[np.argmax(predictions[0])]
+    
+    confidence = round(100 * (np.max(predictions[0])), 2)
+    
+    return(predicted_class, confidence)
+
+
+plt.figure(figsize=(15, 15))
+for images, labels in test_dataset.take(1):
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        
+        predicted_class, confidence = predict(model, images[i].numpy())
+        actual_class = class_names[labels[i]] 
+        
+        plt.title(f"Actual: {actual_class},\n Predicted: {predicted_class}.\n Confidence: {confidence}%")
+        
+        plt.axis("off")
+
+
+model.save('models')
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# bell-pepper-disease-classification
-bell pepper disease classification
+## bell pepper disease classification
+
+
+## Dataset 
+[Datset Link](https://www.kaggle.com/arjuntejaswi/plant-village)
+
+
+## How To Run
+```
+Step 1 : git clonehttps://github.com/sujitmandal/bell-pepper-disease-classification.git
+
+Step 2 : cd bell-pepper-disease-classification
+
+Step 3 : pip install -r requirements.txt or pip3 install -r requirements.txt
+
+Step 4 : python py_excel.py Excel_file
+```
+
+## Required Package's or Librarie's:
+
+```
+pip install numpy
+```
+[numpy Link](https://pypi.org/project/numpy/)
+
+```
+pip install tensorflow
+```
+[tensorflow Link](https://pypi.org/project/tensorflow/)
+
+```
+pip install matplotlib
+```
+[matplotlib Link](https://pypi.org/project/matplotlib/)
